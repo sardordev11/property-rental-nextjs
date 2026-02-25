7028321a3f59dca2fe88b39f7f66cb9283ba71ff
Sardor Khadjimatov <sardorkhadjimatov@MacBook-Pro.local>
2026-02-25 09:43:21 +0900

    feat: fetch property details with server component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,5 @@
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertyDetails";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Link from "next/link";
@@ -24,7 +25,7 @@ const PropertyPage = async ({ params }) => {
       <section class="bg-blue-50">
         <div class="container m-auto py-10 px-6">
           <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            {/* Property Info */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
