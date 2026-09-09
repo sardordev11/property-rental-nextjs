e764c85b40fcb168d099a7d206cee2d6a87b2acb
Sardor Khadjimatov <sardorkhadjimatov@MacBook-Pro.local>
2026-09-09 10:02:19 +0900

    feat: serialize lean documents

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import profileDefault from "@/assets/images/profile.png";
 import ProfileProperties from "@/components/ProfileProperties";
+import { convertToSerializableObject } from "@/utils/convertToObject";
 
 const ProfilePage = async () => {
   await connectDB();
@@ -16,14 +17,15 @@ const ProfilePage = async () => {
     throw new Error("User ID id required");
   }
 
-  const properties = await Property.find({ owner: userId }).lean();
+  const propertiesDocs = await Property.find({ owner: userId }).lean();
+  const properties = propertiesDocs.map(convertToSerializableObject);
 
   return (
     <section className="bg-blue-50">
       <div className="container m-auto py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <h1 className="text-3xl font-bold mb-4">Your Profile</h1>
-          <div className="flex flex-col md:flex-row">
+          <div className="flex fl ex-col md:flex-row">
             <div className="md:w-1/4 mx-20 mt-10">
               <div className="mb-4">
                 <Image
--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -1,13 +1,14 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const ProfileProperties = ({ properties: initialProperties }) => {
   const [properties, setProperties] = useState(initialProperties);
 
   return properties.map((property) => (
     <div key={property._id} className="mb-10">
-      <a href="/property.html">
+      <Link href={`/properties/${property._id}`}>
         <Image
           className="h-32 w-full rounded-md object-cover"
           src={property.images[0]}
@@ -15,7 +16,7 @@ const ProfileProperties = ({ properties: initialProperties }) => {
           width={1000}
           height={200}
         />
-      </a>
+      </Link>
       <div className="mt-2">
         <p className="text-lg font-semibold">{property.name}</p>
         <p className="text-gray-600">
