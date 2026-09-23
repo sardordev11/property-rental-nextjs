5c38ca15f4b8bfcd732ab2b2de058535fdfa1a53
Sardor <sardorxadjimatov95@gmail.com>
2026-09-23 10:53:29 +0900

    feat: add bookmark status check

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,5 +1,7 @@
 "use client";
+import { useState, useEffect } from "react";
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 import { toast } from "react-toastify";
 import { FaBookmark } from "react-icons/fa";
 import { useSession } from "next-auth/react";
@@ -7,6 +9,22 @@ import { useSession } from "next-auth/react";
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) toast.error(res.error);
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
 
   const handleClick = async () => {
     if (!userId) {
@@ -16,11 +34,24 @@ const BookmarkButton = ({ property }) => {
 
     bookmarkProperty(property._id).then((res) => {
       if (res.error) return toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
       toast.success(res.message);
     });
   };
 
-  return (
+  if (loading) {
+    return <p className="text-cente">Loading...</p>;
+  }
+
+  return isBookmarked ? (
+    <button
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center 
+    justify-center"
+      onClick={handleClick}
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center 
     justify-center"
